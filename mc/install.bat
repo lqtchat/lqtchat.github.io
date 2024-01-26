1cd5640e64263f812831c622201f117b30f1557c
lqtchat <151303545+lqtchat@users.noreply.github.com>
2024-01-26 10:42:56 +0800

    Create install.bat

---
+++ mc/install.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Installing Original Game Version
+cmcl.exe install 1.20.2
+echo Downloading Forge Installer
+wget.exe https://fs-im-kefu.7moor-fs1.com/29397395/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1706236094197/forge-1.20.2-48.1.0-installer.jar -O forge-1.20.2-48.1.0-installer.jar
+wget.exe https://fs-im-kefu.7moor-fs1.com/29397395/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1706236094032/ForgeCLI-1.0.1.jar -O ForgeCLI-1.0.1.jar
+echo Installing Forge
+java -jar ForgeCLI-1.0.1.jar --installer forge-1.20.2-48.1.0-installer.jar --target ..\.minecraft\
+echo OK
+exit
